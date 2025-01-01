383c7a9888930af30b1ecdd1d189ccd9962ed6fe
priyadarshi7 <priyadarshisatyakam3@gmail.com>
2025-01-01 21:34:16 +0530

    conflict-resolved

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,7 +1,15 @@
-import mongoose from "mongoose";
+import mongoose, {Schema, Document, Model} from "mongoose";
+
+export interface IUser extends Document {
+    _id: mongoose.Types.ObjectId;
+    username: string;
+    email: string;
+    password: string;
+    enrolledCourses: mongoose.Types.ObjectId[]; // Array of Course IDs
+}
 
 //Schema
-const UserSchema = new mongoose.Schema({
+const UserSchema: Schema = new mongoose.Schema({
     username:{
         type:String,
         required:[true,'username is required'],
@@ -16,6 +24,7 @@ const UserSchema = new mongoose.Schema({
         type:String,
         required:[true,'password is required'],
     },
+    enrolledCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
     // salt:{
     //     type:String,
     //     select:false,
@@ -27,4 +36,4 @@ const UserSchema = new mongoose.Schema({
 },{timestamps:true});
 
 //model
-export const UserModel = mongoose.model("user",UserSchema);
+export const UserModel = mongoose.model<IUser>("user",UserSchema);
